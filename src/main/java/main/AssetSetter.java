3595a5c0f8aba60e558f8eaec5fbea6b24c7ddc3
MacNikSipeykin <niksipeykin@gmail.com>
2025-02-01 14:10:44 +0300

    Add Stackable Items

--- src/main/java/main/AssetSetter.java
+++ src/main/java/main/AssetSetter.java
@@ -42,6 +42,18 @@ public class AssetSetter {
     gp.obj[mapNum][i].worldX = gp.tileSize*21;
     gp.obj[mapNum][i].worldY = gp.tileSize*22;
     i++;
+    gp.obj[mapNum][i] = new OBJ_Potion_Red(gp);
+    gp.obj[mapNum][i].worldX = gp.tileSize*20;
+    gp.obj[mapNum][i].worldY = gp.tileSize*22;
+    i++;
+    gp.obj[mapNum][i] = new OBJ_Potion_Red(gp);
+    gp.obj[mapNum][i].worldX = gp.tileSize*20;
+    gp.obj[mapNum][i].worldY = gp.tileSize*21;
+    i++;
+    gp.obj[mapNum][i] = new OBJ_Potion_Red(gp);
+    gp.obj[mapNum][i].worldX = gp.tileSize*20;
+    gp.obj[mapNum][i].worldY = gp.tileSize*20;
+    i++;
 
 
     mapNum = 1;
--- src/main/java/main/UI.java
+++ src/main/java/main/UI.java
@@ -409,6 +409,23 @@ public class UI {
             }
             g2.drawImage(entity.inventory.get(i).down1, slotX, slotY, null);
 
+            //display amount
+            if (entity == gp.player && entity.inventory.get(i).amount > 1){
+                g2.setFont(g2.getFont().deriveFont(32f));
+                int amountX;
+                int amountY;
+
+                String s = "" + entity.inventory.get(i).amount;
+                amountX = getXForAlignToRightText(s, slotX + 44);
+                amountY = slotY + gp.tileSize;
+
+                //shadow
+                g2.setColor(new Color(60, 60, 60));
+                g2.drawString(s, amountX, amountY);
+                //number
+                g2.setColor(Color.white);
+                g2.drawString(s, amountX -3, amountY-3);
+            }
             slotX += slotSize;
 
             if (i == 4 || i == 9 || i == 14 ){
@@ -835,15 +852,15 @@ public class UI {
                     currentDialogue = "You need more coin to buy that!";
                     drawDialogueScreen();
                 }
-                else if (gp.player.inventory.size() == gp.player.maxInventorySize){
-                    subState = 0;
+                else {
+                    if (gp.player.canObtainItem(npc.inventory.get(itemIndex))){
+                        gp.player.coin -= npc.inventory.get(itemIndex).price;
+                    }
+                    else{
+                        subState = 0;
                     gp.gameState = gp.dialogueState;
                     currentDialogue = "You can't carry any more!";
-                    drawDialogueScreen();
-                }
-                else {
-                    gp.player.coin -= npc.inventory.get(itemIndex).price;
-                    gp.player.inventory.add(npc.inventory.get(itemIndex));
+                    }
                 }
             }
         }
@@ -899,7 +916,12 @@ public class UI {
                     currentDialogue = "You can't sell an equipped item!";
                 }
                 else {
-                    gp.player.inventory.remove(itemIndex);
+                    if (gp.player.inventory.get(itemIndex).amount > 1){
+                        gp.player.inventory.get(itemIndex).amount--;
+                    }
+                    else {
+                        gp.player.inventory.remove(itemIndex);
+                    }
                     gp.player.coin += price;
                 }
             }
